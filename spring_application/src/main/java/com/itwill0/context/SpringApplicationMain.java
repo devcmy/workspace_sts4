5347064199a669de00c0970b85958aaf637b8b1c
itwillPC <devcmy136@gmail.com>
2023-02-21 14:50:30 +0900

    **

--- spring_application/src/main/java/com/itwill0/context/SpringApplicationMain.java
+++ spring_application/src/main/java/com/itwill0/context/SpringApplicationMain.java
@@ -7,10 +7,19 @@ public class SpringApplicationMain {
 
 	public static void main(String[] args) {
 		/*
-		 * Spring Container 객체생성(얘네들이 대신 객체생성)
+		 * Spring Container 객체생성(얘네들이 대신 객체생성(우리가직접작성x)
 		 *  - ApplicationContext[BeanFactory]객체생성
 		 */
+		
+		/*
+		 * 1. Spring Bean 설정파일을 읽어서 SpringContainer 객체생성
+		 */
+		System.out.println("---------------------ApplicationContext 생성시작-----------------------------------------------------");
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/itwill0/context/0.application_context.xml");
+		System.out.println("---------------------ApplicationContext 생성끝-----------------------------------------------------");
+		
+		
+		
 		
 	}
 
