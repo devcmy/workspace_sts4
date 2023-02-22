a719db89178a70888d63d564cddba79db7bf153c
itwillPC <devcmy136@gmail.com>
2023-02-22 10:54:54 +0900

    **

--- spring_application/src/main/java/com/itwill1/bean/create/SpringBootBeanCreateApplicationMain.java
+++ spring_application/src/main/java/com/itwill1/bean/create/SpringBootBeanCreateApplicationMain.java
@@ -3,12 +3,18 @@ package com.itwill1.bean.create;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 /*
  * 현재 클래스가 위치하는 패키지와 하위패키지의 클래스의 @Annotation을 스캔해서 
  * 초기화(객체생성, 의존성 주입)	
  */
 @SpringBootApplication
+/*
+ * @ComponentScan(basePackages = {"com.itwill.guest"})
+ * @ComponentScan(value = {"com.itwill.guest"})
+ */
+@ComponentScan(basePackages = {"com.itwill.guest","com.itwill.user"}) //값만 넣어주면 default annotation이 value로 되어 있다. 여러주소(개) 쓰러면 윗방식과 같이 작성할것.
 public class SpringBootBeanCreateApplicationMain {
 
 	public static void main(String[] args) {
