7c4a2f3e2962ac84a1181d7466095c3f6c674ec9
itwillPC <devcmy136@gmail.com>
2023-02-22 13:53:49 +0900

    **

--- spring_application/src/main/java/com/itwill/user/UserServiceImpl.java
+++ spring_application/src/main/java/com/itwill/user/UserServiceImpl.java
@@ -2,21 +2,30 @@ package com.itwill.user;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-@Service(value = "userService")
+@Service("userService")
 public class UserServiceImpl implements UserService {
-
+	private UserDao userDao; //의존성
+	
 	public UserServiceImpl() {
 		System.out.println("#### UserServiceImpl() : 디폴트생성자호출");
 
 	}
-
 	
+	
+	@Autowired
+	public void setUserDao(UserDao userDao) {
+		System.out.println("#### UserServiceImpl.setUserDao(UserDao userDao) : 메쏘드호출");
+		this.userDao = userDao;
+	}
+
+
 
 	public int create(User user) throws Exception {
-
+		userDao.create(user);
 		System.out.println("#### UserServiceImpl : create() 호출");
-
+		
 		return 0;
 	}
 
--- spring_application/src/main/java/com/itwill2/bean/di/SpringBootBeanDependencyInjectionMain.java
+++ spring_application/src/main/java/com/itwill2/bean/di/SpringBootBeanDependencyInjectionMain.java
@@ -1,29 +1,37 @@
 package com.itwill2.bean.di;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.itwill.guest.GuestService;
+import com.itwill.user.UserService;
 
 
 @SpringBootApplication
-@ComponentScan({"com.itwill.guest"})
+@ComponentScan({"com.itwill.guest","com.itwill.user"})
 public class SpringBootBeanDependencyInjectionMain {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("-----Spring Container 초기화 시작[ApplicationContext객체생성 시작]-------");
 		ApplicationContext applicationContext = SpringApplication.run(SpringBootBeanDependencyInjectionMain.class, args);
+		
+		/* SpringApplication application = new SpringApplication(SpringBootBeanDependencyInjectionMain.class);
+		 application.setBannerMode(Banner.Mode.OFF);
+		 ApplicationContext applicationContext=application.run(args); */
+		 
 		System.out.println("-----Spring Container 초기화 끝[ApplicationContext객체생성 끝]-------");
 		//Injection없이 작업
 		GuestService guestService = applicationContext.getBean(GuestService.class);
 		
-		//guestService.selectAll();
 		System.out.println("------Constructor Injection(생성자 주입)----------");
+		guestService.selectAll();
 		
 		System.out.println("------Setter      Injection(생성자 주입)----------");
-		
+		UserService userService = (UserService)applicationContext.getBean(UserService.class);
+		userService.create(null);
 		
 		
 		
