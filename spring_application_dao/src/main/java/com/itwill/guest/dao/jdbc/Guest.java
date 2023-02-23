e825c6358f6060e2ea37fbbd0beb69e5d26cd822
itwillPC <devcmy136@gmail.com>
2023-02-23 14:13:31 +0900

    **

--- spring_application_dao/src/main/java/com/itwill/guest/dao/jdbc/Guest.java
+++ spring_application_dao/src/main/java/com/itwill/guest/dao/jdbc/Guest.java
@@ -1,85 +1,18 @@
 package com.itwill.guest.dao.jdbc;
-/*
-이름             널?       유형             
--------------- -------- -------------- 
-GUEST_NO       NOT NULL NUMBER(10)     
-GUEST_NAME     NOT NULL VARCHAR2(50)   
-GUEST_DATE     NOT NULL DATE           
-GUEST_EMAIL             VARCHAR2(50)   
-GUEST_HOMEPAGE          VARCHAR2(50)   
-GUEST_TITLE    NOT NULL VARCHAR2(100)  
-GUEST_CONTENT  NOT NULL VARCHAR2(4000) 
-*/
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Guest {
-	private int guest_no;
-	private String guest_name;
+	private int guest_no; //이름이 다르면 BeanPropertyRowMapper 자동으로 못만들어준다(객체생성은 o,  데이터 받아오진못함) -> custom해줘야한다.
+	private String guest_name; //Camel케이스로 작성해도, snake케이스로 변환해준다.
 	private String guest_date;
 	private String guest_email;
 	private String guest_homepage;
 	private String guest_title;
 	private String guest_content;
-	public Guest() {
-	}
-	public Guest(int guest_no, String guest_name, String guest_date, String guest_email, String guest_homepage,
-			String guest_title, String guest_content) {
-		super();
-		this.guest_no = guest_no;
-		this.guest_name = guest_name;
-		this.guest_date = guest_date;
-		this.guest_email = guest_email;
-		this.guest_homepage = guest_homepage;
-		this.guest_title = guest_title;
-		this.guest_content = guest_content;
-	}
-	public int getGuest_no() {
-		return guest_no;
-	}
-	public void setGuest_no(int guest_no) {
-		this.guest_no = guest_no;
-	}
-	public String getGuest_name() {
-		return guest_name;
-	}
-	public void setGuest_name(String guest_name) {
-		this.guest_name = guest_name;
-	}
-	public String getGuest_date() {
-		return guest_date;
-	}
-	public void setGuest_date(String guest_date) {
-		this.guest_date = guest_date;
-	}
-	public String getGuest_email() {
-		return guest_email;
-	}
-	public void setGuest_email(String guest_email) {
-		this.guest_email = guest_email;
-	}
-	public String getGuest_homepage() {
-		return guest_homepage;
-	}
-	public void setGuest_homepage(String guest_homepage) {
-		this.guest_homepage = guest_homepage;
-	}
-	public String getGuest_title() {
-		return guest_title;
-	}
-	public void setGuest_title(String guest_title) {
-		this.guest_title = guest_title;
-	}
-	public String getGuest_content() {
-		return guest_content;
-	}
-	public void setGuest_content(String guest_content) {
-		this.guest_content = guest_content;
-	}
-	@Override
-	public String toString() {
-		return "Guest [guest_no=" + guest_no + ", guest_name=" + guest_name + ", guest_date=" + guest_date
-				+ ", guest_email=" + guest_email + ", guest_homepage=" + guest_homepage + ", guest_title=" + guest_title
-				+ ", guest_content=" + guest_content + "]";
-	}
-	
-	
-	
 }
--- spring_application_dao/src/main/java/com/itwill/guest/dao/jdbctemplate/SpringBootJdbcTemplateMain.java
+++ spring_application_dao/src/main/java/com/itwill/guest/dao/jdbctemplate/SpringBootJdbcTemplateMain.java
@@ -2,6 +2,7 @@ package com.itwill.guest.dao.jdbctemplate;
 
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,14 +23,20 @@ public class SpringBootJdbcTemplateMain {
 		
 		System.out.println("1. JdbcTemplate객체얻기");
 		/***********************************************************************************************
-		 * SELECT --> queryForObject 반환타입[String,Wrapper,DTO(Bean)] //Bean/list/map/wrapper/string
+		 * SELECT --> queryForObject 반환타입[String,Wrapper,DTO(Bean)] //Bean/list/map/wrapper(ex:Integer처럼 자동wrapper가아닌 기술해야함.)/string
 		 ***********************************************************************************************/
 		/*
-		 * queryForObject[반환타입 DTO]
+		 * queryForObject[반환타입 DTO인것]
 		 */
 		String selectDtoSql = "select * from guest where guest_no=?";
 		Object[] paramArray1 = {263}; //이렇게 초기화하거나 -> 다이렉트로 안들어감
 		Object[] paramArray2 = new Object[] {263}; //혹은 이렇게 초기화하거나
+		/*
+		 <<BeanPropertyRowMapper 클래스>>
+		 	ResultSet객체로부터 데이터를 컬럼이름으로 get해서 
+		 	Guest객체 생성후 테이블컬럼이름과 동일한 이름의 
+		 	Guest객체의 Property(멤버변수)에 대입해주는 클래스.
+		 */
 		BeanPropertyRowMapper<Guest> guestBeanPropertyRowMapper = new BeanPropertyRowMapper<Guest>(Guest.class);
 		//rowMapper가 뽑아서 자동 mapping 해줌
 		
@@ -37,16 +44,26 @@ public class SpringBootJdbcTemplateMain {
 		
 		//rs를 뽑아서 1개의 rs 데이터와 Guest 객체를 만든후 bean의 property(Set)와 mapping함
 		Guest guest = jdbcTemplate.queryForObject(selectDtoSql,
-												new Object[]{263}, //인자가 많을수있으니, 배열로 넣음.
+												new Object[]{263}, //인자가 많을수있으니, 배열로 넣음. object는 각 테이블의 변수들이 다양하기때문에 최상위인 Object로 다 받음.
 												new int[] {Types.INTEGER}, //263 의 타입을 상수로 기술해달라.
 												guestBeanPropertyRowMapper) ; //선언없이 Direct로 집어넣는방법
+		//한개 반환
 		System.out.println("queryForObject[Guest]:"+guest);
+		/***********************************************************************************************
+		 * SELECT --> query 반환타입[List<Guest or User>] (list인경우 query)
+		 ***********************************************************************************************/
 		/*
-		 * SELECT --> query 반환타입[List<Guest,User>] 해당 리스트를 받을때 query를 쓰면된다. 
+		 * SELECT --> query 반환타입[List<Guest>] 해당 리스트를 받을때 query를 쓰면된다. 
 		 */
+		String selectDtoListSql = "select * from guest";
+		List<Guest> guestList = jdbcTemplate.query(selectDtoListSql,guestBeanPropertyRowMapper); //리스트 반환
+		System.out.println("query[List<Guest>] : "+guestList);
 		/*
 		 * DML(update,delete,insert) --> update 반환타입[ rowCount(int) ] 
 		 */
+		String insertSql="insert into guest values(guest_no_seq.nextval,?,sysdate,?,?,?,?)";
+		String updateSql="update guest set  guest_name=?, guest_email=?, guest_homepage=?, guest_title=?, guest_content=? where guest_no=?";
+		String deleteSql="delete from guest where guest_no=?";
 	}
 
 }
