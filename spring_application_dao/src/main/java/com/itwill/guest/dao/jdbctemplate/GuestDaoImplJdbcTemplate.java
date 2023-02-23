0c5179e932d544e3a122ee3bfa3e69d9605ee650
itwillPC <devcmy136@gmail.com>
2023-02-23 15:32:36 +0900

    **

--- spring_application_dao/src/main/java/com/itwill/guest/dao/jdbctemplate/GuestDaoImplJdbcTemplate.java
+++ spring_application_dao/src/main/java/com/itwill/guest/dao/jdbctemplate/GuestDaoImplJdbcTemplate.java
@@ -10,15 +10,20 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class GuestDaoImplJdbcTemplate implements GuestDao { //여전히 jdbc template는 sql파일이 필요로 하다.
-
+	//appalicationContext 가 초기화될때,  Datasource와 jdbctemplate을 생성함(datasource 객체를 찾아서 injection주입함)
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	@Override
 	public List<Guest> selectAll() throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return jdbcTemplate.query(GuestSQL.GUEST_SELECT_ALL, new BeanPropertyRowMapper<Guest>(Guest.class));
 	}
 
 	@Override
