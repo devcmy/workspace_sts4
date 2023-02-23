0a14a473dfbdca342062a73211497594d11ee68f
itwillPC <devcmy136@gmail.com>
2023-02-23 10:03:05 +0900

    **

--- spring_application_dao/src/test/java/com/itwill/datasource/SpringBootDataSourceApplicationMainTest.java
+++ spring_application_dao/src/test/java/com/itwill/datasource/SpringBootDataSourceApplicationMainTest.java
@@ -8,13 +8,13 @@ import javax.sql.DataSource;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootApplication
 @SpringBootTest
 class SpringBootDataSourceApplicationMainTest {
 	@Autowired
 	DataSource dataSource;
-	
-	
 	
 	@Test
 	void contextLoad() {
--- spring_application_dao/src/test/java/com/itwill/guest/GuestDaoImplTest.java
+++ spring_application_dao/src/test/java/com/itwill/guest/GuestDaoImplTest.java
@@ -2,43 +2,23 @@ package com.itwill.guest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest 
+
+@SpringBootApplication
+@SpringBootTest
 class GuestDaoImplTest {
+	
 	@Autowired
 	GuestDao guestDao;
 	
-	@Test
-	void contextLoad() {
-		
-	}
-	
 	
 	@Test
-	void testSelectAll() throws Exception{
-		System.out.println(guestDao.selectAll());
-	}
-	/*
-	@Test
-	void testSelectByNo() {
-		fail("Not yet implemented");
+	void testSelectAll() throws Exception {
+		guestDao.selectAll();
 	}
 
-	@Test
-	void testInsertGuest() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testUpdateGuest() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testDeleteGuest() {
-		fail("Not yet implemented");
-	}
-	*/
 }
