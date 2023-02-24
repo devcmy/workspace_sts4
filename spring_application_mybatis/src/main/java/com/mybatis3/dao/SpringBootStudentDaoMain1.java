c4f4132ddbf5dc63a57c0639409c8a345424fbe4
itwillPC <devcmy136@gmail.com>
2023-02-24 14:33:26 +0900

    **4교시 finish~

--- spring_application_mybatis/src/main/java/com/mybatis3/dao/SpringBootStudentDaoMain1.java
+++ spring_application_mybatis/src/main/java/com/mybatis3/dao/SpringBootStudentDaoMain1.java
@@ -1,5 +1,6 @@
 package com.mybatis3.dao;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,12 +12,19 @@ public class SpringBootStudentDaoMain1 {
 	public static void main(String[] args) {
 		ApplicationContext appicationContext=
 				SpringApplication.run(SpringBootStudentDaoMain1.class, args);
-		StudentDao studentDao =appicationContext.getBean(StudentDao.class);
+		//SqlSession sqlSession = appicationContext.getBean(SqlSession.class);
+		//StudentDao studentDao =appicationContext.getBean(StudentDao.class); //DAO객체를 얻는 작업
+		StudentDao studentDao = (StudentDao)appicationContext.getBean(StudentDao.class);
 		System.out.println("---------findStudentById-----------------------------");
+		System.out.println(studentDao.findStudentById(1));
 		System.out.println("---------findAllStudents-----------------------------");
+		System.out.println(studentDao.findAllStudents());
 		System.out.println("---------findStudentNameById-------------------------");
+		
 		System.out.println("---------findStudentNameList-------------------------");
+		
 		System.out.println("---------findStudentByIdResultMap--------------------");
+		
 		System.out.println("---------findAllStudentsResultMap--------------------");
 		
 		
--- spring_application_mybatis/src/main/java/com/mybatis3/dao/StudentDao.java
+++ spring_application_mybatis/src/main/java/com/mybatis3/dao/StudentDao.java
@@ -14,12 +14,62 @@ import org.springframework.stereotype.Repository;
 
 import com.mybatis3.domain.Student;
 
-
+@Repository
 public class StudentDao {
 	
+	@Autowired
+	private SqlSession sqlSession;
 	
 	public StudentDao() {
 	}
+	
+	/**************************************************
+	 * SELECT
+	 **************************************************/
+	/*
+	 * A.select sql의결과타입이 DTO,VO,Domain객체인경우 resultType : DTO,VO,Domain
+	 */
+	public Student findStudentById(Integer studId) {
+		return sqlSession.selectOne("findStudentById",studId);
+	}
+
+	public List<Student> findAllStudents() {
+		return sqlSession.selectList("findAllStudents");
+	}
+
+	/*
+	 * select sql의결과타입이 Wrapper,String 객체인경우 resultType : Wrapper,String
+	 */
+	public String findStudentNameById(Integer userId) {
+		return null;
+	}
+
+	public List<String> findStudentNameList() {
+		return null;
+	}
+
+	/*
+	 * B.select sql의결과타입이 DTO,VO,Domain객체인경우 resultMap : DTO,VO,Domain
+	 */
+	public Student findStudentByIdResultMap(Integer studId) {
+		return null;
+	}
+
+	public List<Student> findAllStudentsResultMap() {
+		return null;
+	}
+
+	public List<Student> findStudentByIdRangeParamMap(HashMap idRangeMap) {
+		return null;
+	}
+	public List<Student> findStudentsThreeParamMap(HashMap studentsMap) {
+		return null;
+	}
+	public int updateStudentParamMap(Map studentMap) {
+		return 0;
+	}
+	
+	
 	/**************************************************
 	 * 결과데이타를 Map(HashMap)에 담아서 반환할수있다
 	 ***************************************************/
@@ -102,50 +152,6 @@ public class StudentDao {
 		return null;
 	}
 
-	/**************************************************
-	 * SELECT
-	 **************************************************/
-	/*
-	 * A.select sql의결과타입이 DTO,VO,Domain객체인경우 resultType : DTO,VO,Domain
-	 */
-	public Student findStudentById(Integer studId) {
-		return null;
-	}
-
-	public List<Student> findAllStudents() {
-		return null;
-	}
-
-	/*
-	 * select sql의결과타입이 Wrapper,String 객체인경우 resultType : Wrapper,String
-	 */
-	public String findStudentNameById(Integer userId) {
-		return null;
-	}
-
-	public List<String> findStudentNameList() {
-		return null;
-	}
-
-	/*
-	 * B.select sql의결과타입이 DTO,VO,Domain객체인경우 resultMap : DTO,VO,Domain
-	 */
-	public Student findStudentByIdResultMap(Integer studId) {
-		return null;
-	}
-
-	public List<Student> findAllStudentsResultMap() {
-		return null;
-	}
-
-	public List<Student> findStudentByIdRangeParamMap(HashMap idRangeMap) {
-		return null;
-	}
-	public List<Student> findStudentsThreeParamMap(HashMap studentsMap) {
-		return null;
-	}
-	public int updateStudentParamMap(Map studentMap) {
-		return 0;
-	}
+	
 
 }
