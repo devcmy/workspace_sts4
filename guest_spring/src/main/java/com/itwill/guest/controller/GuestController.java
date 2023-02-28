f8b862e536be143777c40c16da0c89e327e70657
M\Motte <devcmy136@gmail.com>
2023-03-01 00:07:31 +0900

    수정중

--- guest_spring/src/main/java/com/itwill/guest/controller/GuestController.java
+++ guest_spring/src/main/java/com/itwill/guest/controller/GuestController.java
@@ -1,11 +1,26 @@
 package com.itwill.guest.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.itwill.guest.Guest;
+import com.itwill.guest.GuestService;
 @Controller
 public class GuestController {
+	@Autowired
+	private GuestService guestService;
+	
 	public GuestController() {
+		
 		System.out.println(">>>GuestController");
+		
 	}
 	@RequestMapping("/guest_main")
 	public String guest_main() {
@@ -13,74 +28,77 @@ public class GuestController {
 
 		return forwardPath;
 	}
-
+	@RequestMapping("/guest_list")
 	public String guest_list() {
-		String forwardPath = "";
+		String forwardPath = "forward:/WEB-INF/views/guest_list.jsp";
 
 		return forwardPath;
 	}
-
+	@RequestMapping("/guest_error")
 	public String guest_error() {
-		String forwardPath = "";
+		String forwardPath = "forward:/WEB-INF/views/guest_error.jsp";
 
 		return forwardPath;
 	}
-
-	public String guest_modify_action_get() {
-		String forwardPath = "";
+	@GetMapping("/guest_modify_action")
+	public String guest_modify_action_get() throws Exception {
+		String forwardPath = "redirect:guest_main.jsp";
+		return forwardPath;
+	}
+	@PostMapping("/guest_modify_action")
+	public String guest_modify_action(@ModelAttribute Guest guest) throws Exception{
+		guestService.updateGuest(guest);
+		String forwardPath = "forward:/WEB-INF/views/guest_view.jsp";
 
 		return forwardPath;
 	}
-
-	public String guest_modify_action() {
-		String forwardPath = "";
-
-		return forwardPath;
-	}
-
-	public String guest_modify_get() {
-		String forwardPath = "";
+	@GetMapping("/guest_modify_form")
+	public String guest_modify_get() throws Exception {
+		
+		String forwardPath = "redirect:guest_main.jsp";
 
 		return forwardPath;
 	}
-
+	@PostMapping("/guest_modify_form")
 	public String guest_modify_form() {
-		String forwardPath = "";
+		String forwardPath = "forward:/WEB-INF/views/guest_modify_form.jsp";
+		return forwardPath;
+	}
+	@GetMapping("/guest_remove_action")
+	public String guest_remove_action_get() throws Exception {
+		String forwardPath = "redirect:guest_main.jsp";
 		return forwardPath;
 	}
 
-	public String guest_remove_action_get() {
-		String forwardPath = "";
+	@RequestMapping("/guest_remove_action")
+	public String guest_remove_action(@ModelAttribute Guest guest) throws Exception {
+		String forwardPath = "redirect:guest_list.jsp";
+		guestService.deleteGuest(Integer.parseInt("guest_no"));
+		
 		return forwardPath;
 	}
-
-	
-	public String guest_remove_action() {
-		String forwardPath = "";
-
-		return forwardPath;
-	}
-
+	@RequestMapping("/guest_view")
 	public String guest_view() {
-		String forwardPath = "";
+		String forwardPath = "forward:/WEB-INF/views/guest_view.jsp";
 
 		return forwardPath;
 	}
-
-	public String guest_write_action_get() {
-		String forwardPath = "";
-		return forwardPath;
-	}
-
-	public String guest_write_action() {
-		String forwardPath = "";
-
-		return forwardPath;
-	}
-
 	
-	public String guest_write() {
-		String forwardPath = "";
+	@GetMapping("/guest_write_action")
+	public String guest_write_action_get() {
+		String forwardPath = "forward:/WEB-INF/views/guest_write_form.jsp";
+		return forwardPath;
+	}
+	@PostMapping("/guest_write_action")
+	public String guest_write_action(@ModelAttribute Guest guest) {
+		String forwardPath = "forward:/WEB-INF/views/guest_view.jsp";
+
+		return forwardPath;
+	}
+
+	@RequestMapping("/guest_write_form")
+	public String guest_write(Guest guest) {
+		String forwardPath = "forward:/WEB-INF/views/guest_write_form.jsp";
 		return forwardPath;
 
 	}
