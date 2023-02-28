bb5ab6dc45baa229ab9696f4b99ddc5054c8a0b5
itwillPC <devcmy136@gmail.com>
2023-02-28 16:37:41 +0900

    **

--- spring_web_application/src/main/java/com/itwill/controller/RequestMappingGETPOSTController.java
+++ spring_web_application/src/main/java/com/itwill/controller/RequestMappingGETPOSTController.java
@@ -1,13 +1,22 @@
 package com.itwill.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class RequestMappingGETPOSTController {
-
+	
+	//@RequestMapping(method = RequestMethod.GET)
+	@GetMapping("/login")  //get으로 들어오면, form을 보여주고
 	public String get_login_form() {
-		return "";
+		return "forward:/WEB-INF/views/get_login_form.jsp";
 	}
-
+	@PostMapping("/login") //post로 들어오면 action
 	public String post_login_action() {
 
-		return "";
+		return "forward:/WEB-INF/views/get_login_form.jsp";
 	}
 }
