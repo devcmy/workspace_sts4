f23965e1479bf53577aa36201b823497d3ef7e65
M\Motte <devcmy136@gmail.com>
2023-03-02 22:30:35 +0900

    과제 끝

--- user_spring/src/main/java/com/itwill/user/controller/UserController.java
+++ user_spring/src/main/java/com/itwill/user/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,9 +41,18 @@ public class UserController {
 		return forward_path;
 	}
 	@RequestMapping("/user_write_action")
-	public String user_write_action_post(User user) throws Exception {
-		userService.create(user);
-		String forward_path = "user_main";
+	public String user_write_action_post(User user, Model model) throws Exception {
+		String forward_path = "";
+		int CreateR = userService.create(user);
+		if (CreateR == -1) {
+			String msg = user.getUserId()+ "는 이미 사용중인 아이디입니다.";
+			model.addAttribute("msg",msg);
+			model.addAttribute("fuser",user);
+			forward_path = "user_write_form";
+		}else {
+			model.addAttribute("user",user);
+			forward_path = "redirect:user_login_form";
+		}
 		return forward_path;
 	}
 	@RequestMapping("/user_login_form")
@@ -51,12 +61,25 @@ public class UserController {
 		return forward_path;
 	}
 	@RequestMapping("/user_login_action")
-	public String user_login_action_post(User user, HttpSession session) throws Exception {
-		int loginResult = userService.login(user.getUserId(), user.getPassword());
-		String forwardPath = "user_main";
+	public String user_login_action_post(User user, HttpSession session,Model model) throws Exception {
+		String userId = user.getUserId();
+		String pass = user.getPassword();
+		int loginResult = userService.login(userId,pass);
+		String forwardPath = "";
 		
-		if (loginResult == 2) {
-			session.setAttribute("sUserId", user.getUserId());
+		if(loginResult == 0) {
+			String msg1=userId+" 는 존재하지 않는 아이디입니다.";
+			model.addAttribute("msg1",msg1);
+			model.addAttribute("fuser",user);
+			forwardPath = "user_login_form";
+		}else if (loginResult == 1) {
+			String msg2="비밀번호가 일치하지 않습니다.";
+			model.addAttribute("msg2",msg2);
+			model.addAttribute("fuser",user);
+			forwardPath = "user_login_form";
+		}else if (loginResult == 2) {
+			session.setAttribute("sUserId", userId);
+			forwardPath = "user_main";
 		}
 		return forwardPath;
 	}
@@ -73,40 +96,48 @@ public class UserController {
 		return forwardPath;
 	}
 
-
-	public String user_modify_form_post(HttpSession session, HttpServletRequest request) throws Exception {
+	@RequestMapping("/user_modify_form")
+	public String user_modify_form_post(HttpSession session, HttpServletRequest request,Model model) throws Exception {
 		/************** login check **************/
 		session = request.getSession();
 		String loginUser = (String)session.getAttribute("sUserId");
-
-		String forwardPath = "";
-
-		return forwardPath;
-	}
-
-	public String user_modify_action_post(HttpSession session) throws Exception {
-		/************** login check **************/
 		
-		String forwardPath = "";
+		model.addAttribute("loginUser",userService.findUser(loginUser));
+		String forwardPath = "user_modify_form";
+
 		return forwardPath;
 	}
-
-	public String user_remove_action_post(HttpSession session) throws Exception {
+	@RequestMapping("/user_modify_action")
+	public String user_modify_action_post(HttpSession session, HttpServletRequest request, Model model, User user) throws Exception {
 		/************** login check **************/
-		
-		String forwardPath = "";
+		session = request.getSession();
+		String loginUser = (String)session.getAttribute("sUserId");
+		model.addAttribute("loginUser", userService.update(user));
+		String forwardPath = "redirect:user_view";
 		return forwardPath;
 	}
-
-	public String user_logout_action(HttpSession session) {
+	@RequestMapping("/user_remove_action")
+	public String user_remove_action_post(HttpSession session, HttpServletRequest request, Model model) throws Exception {
 		/************** login check **************/
-		
-		String forwardPath = "";
+		session = request.getSession();
+		String loginUser = (String)session.getAttribute("sUserId");
+		model.addAttribute("loginUser",userService.remove(loginUser));
+		session.invalidate();
+		String forwardPath = "redirect:user_main";
 		return forwardPath;
 	}
-
+	@RequestMapping("/user_logout_action")
+	public String user_logout_action(HttpSession session, HttpServletRequest request) {
+		/************** login check **************/
+		session = request.getSession();
+		session.getAttribute("sUserId");
+		session.invalidate();
+		String forwardPath = "redirect:user_main";
+		return forwardPath;
+	}
+	@GetMapping(value = {"user_modify_form","user_modify_action","user_write_action","user_remove_action"})
 	public String user_action_get() {
-		String forwardPath = "";
+		String forwardPath = "redirect:user_main";
 		return forwardPath;
 	}
 
