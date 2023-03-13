de6439c66fde200a2efc24b78fe6fea68253eb26
itwillPC <devcmy136@gmail.com>
2023-03-13 14:41:01 +0900

    jquery

--- jquery/WebContent/jquery-my-0.1.js
+++ jquery/WebContent/jquery-my-0.1.js
@@ -1,7 +1,8 @@
 window.jQuery = function(arg){
 	if(typeof arg=='string'){ //typeof 어떤타입인지
-		
+		/********************** Selector ******************/
 		let elementNodeList = document.querySelectorAll(arg);
+		if(!elementNodeList) elementNodeList=[]; //null과 undefined 체크
 		let jqueryWrapperObject = {
 			'elementNodeList':elementNodeList, //검색해서 모든 node list
 			'css':function(propertyName,propertyValue){  //라이브러리안에 'css' 같은  function들이 json형태로 엄청많이 들어있다.
@@ -12,18 +13,39 @@ window.jQuery = function(arg){
 				return this; //jqueryWrapperObject -> return하는 이유 : chaining code하려고 
 			},
 			'text':function(textArg){
-				for(let i=0;i<this.elementNodeList.length;i++){
-					//this.elementNodeList[i].innerHTML=textArg;
-					this.elementNodeList[i].firstChild.nodeValue=textArg; //fistChild
+				
+				if(textArg){
+					//set text
+					for(let i=0;i<this.elementNodeList.length;i++){
+						//this.elementNodeList[i].innerHTML=textArg;
+						this.elementNodeList[i].firstChild.nodeValue=textArg; //fistChild
+					}
+					return this;
+				}else if(textArg==undefined){
+					//get text
+					let returnText="";
+					for(let i=0;i<this.elementNodeList.length;i++){
+						//this.elementNodeList[i].innerHTML=textArg;
+						returnText += this.elementNodeList[i].firstChild.nodeValue; //fistChild
+					}
+					return returnText;
 				}
-				return this; //jqueryWrapperObject return하는 이유 : chaining code하려고
+				 //jqueryWrapperObject return하는 이유 : chaining code하려고
 			}
 			
 		}
 		return jqueryWrapperObject;
+		
+		}else if(typeof arg=='function'){
+			/*
+			 인자로 대입된 함수를 DOM트리생성직후에 호출되도록
+			 window.onload 이벤트 프로퍼티에 등록
+			 function이면 onload에 등록해봄
+			 */	
+			 window.addEventListener('load',arg);	
+		}
 	}
 	
-}
 window.$=window.jQuery;
 
 
